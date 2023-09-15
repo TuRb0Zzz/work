ad1a1ccc990fe74aa890b0453f6de1f26044e54b
TuRb0Z <evgenynikolaev@inbox.ru>
2023-09-15 21:29:33 +0300

    fix

--- main.c
+++ main.c
@@ -3,38 +3,40 @@ using namespace std;
 
 
 
-	template<class T,size_t size>
-	class Array{
-		private:
-			T* ar;
-		public:
-			Array(){
-				ar = new T[size];
-			}
-			int returnSize(){
-				return size;
-			}
-			T& operator[](int value){
-				return ar[value];
-			}
-			T at(int value) const {
-				return ar[value];
-			};
+    template<class T,size_t size>
+    class Array{
+        private:
+            T* ar;
+        public:
+            Array(){
+                ar = new T[size];
+            }
+            int returnSize(){
+                return size;
+            }
+            T& operator[](int value){
+                return ar[value];
+            }
+            T at(int value) const {
+                return ar[value];
+            };
 
-	};
+    };
 
-	std::ostream& operator<<(std::ostream& out,const Array<int,3>& value){
-		for(size_t i=0; i<3;i++){
-			out<<value.at(i)<<" ";
-		}
-		return out;
-	}
+    template<class T, size_t size>
+    std::ostream& operator<<(std::ostream& out,const Array<T,size>& value)
+    {
+        for(size_t i=0; i<size;i++){
+            out<<value.at(i)<<" ";
+        }
+        return out;
+    }
 
 int main(){
 
-	Array<int,3> test;
-	test[0]=10;
-	cout<<test.returnSize()<<endl;
-	cout<<test<<endl;
-	return 0;
+    Array<int,5> test;
+    test[0]=10;
+    cout<<test.returnSize()<<endl;
+    cout<<test<<endl;
+    return 0;
 }
